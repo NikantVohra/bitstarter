b02f7a80610486ccdd2d3ab1d4fc81e8fd460d6f
NikantVohra <nikant@outlook.com>
2013-10-10 11:34:54 +0000

    test app

--- web.js
+++ web.js
@@ -1,12 +1,13 @@
+var fs = require('fs');
 var express = require('express');
+var buf = require('buffer');
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  var contents_buffer = fs.readfile("index.html");
-	
-  contents = contents_buffer.toString();
-  response.send(contents);
+ var contents_buffer = fs.readFileSync("index.html");
+ var contents = contents_buffer.toString();
+ response.send(contents);
 });
 
 var port = process.env.PORT || 5000;
